acc37cc08d1dd2471cbf8e323c7ae8c861bdf7f5
Lioncat2002 <sankhayan2002@gmail.com>
2022-02-25 23:55:26 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ fn main() {
     let res = requests::get("https://thebulletin.org/doomsday-clock/past-statements/")
         .expect("Failed to fetch website");
     let content=res.text().unwrap();
-    //let match_time=Regex::new(r"IT IS(?: STILL)? (?P<time>\d+)(?: AND A (?P<half>HALF))? (?P<type>MINUTES|SECONDS) TO MIDNIGHT").unwrap();
     
     let content=Soup::new(content);
     let h3=content.tag("h3").find().expect("Error tage <h3> not found!");
